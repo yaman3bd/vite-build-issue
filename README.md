fc1152b20f6a4d44914aa7e7e93ad82e5a13310e
yaman3bd <yaman3bd@gmail.com>
2024-04-14 18:22:50 +0300

    update deps

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Steps to reproduce the issue:
 `npm install`
 `npm run build`
-then inside the dist folder in `vite-build-issue.es.js` search for `.body({`. 
-for example: `ge.body({` click on `ge` to go to its definition, it will be an empty object where it should hold a value containing the `body`
-NOTE:
-`ge` may be a different name after making a new build
+
+this repo has this package which has the issue:
+https://github.com/yaman3bd/test-lib-build
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,29 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { URL, fileURLToPath } from "url";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   publicDir: false,
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true
+    }),
+    tsconfigPaths()
+  ],
   resolve: {
-    alias: [{ find: "@/theme", replacement: fileURLToPath(new URL("./src/theme", import.meta.url)) }]
+    alias: [
+      {
+        find: "@/theme",
+        replacement: fileURLToPath(new URL("./src/theme", import.meta.url))
+      },
+      {
+        find: "@/components",
+        replacement: fileURLToPath(new URL("./src/components", import.meta.url))
+      }
+    ]
   },
   build: {
     lib: {
